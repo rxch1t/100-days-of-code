fe76c1445bb25134cee8a07ecf02f27cc0c52311
rxch1t <naughtymetoday@gmail.com>
2025-10-04 20:56:14 +0530

    day1.c

--- day1.c
+++ day1.c
@@ -10,3 +10,19 @@ int main() {
     printf("Sum is: %d\n", sum);
     return 0;
 }
+
+//WAP to input two numbers and display their sum, difference, product, and quo
+#include <stdio.h>
+int main() {
+    int n1,n2,sum,dif,quo,pdt;
+    printf("enter two numbers:");
+    scanf("%d %d",&n1,&n2);
+
+    sum=n1+n2;
+    dif=n1-n2;
+    quo=n1/n2;
+    pdt=n1*n2;
+
+    printf("sum=%d\n difference=%d\n quotient=%d\n product=%d\n",sum,dif,quo,pdt);
+    return 0;
+}
